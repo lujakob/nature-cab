a09b6d1a6b8161c674cc11a50a61e62987c20f9b
Lukas Jakob <lu.jakob@googlemail.com>
2017-11-03 15:29:20 +0100

    Git remove dist files

--- client/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = require("react");
-const ReactDOM = require("react-dom");
-const BrowserRouter = require("react-router-dom");
-const App_1 = require("./App");
-ReactDOM.render(react_1.default.createElement(BrowserRouter, null,
-    react_1.default.createElement(App_1.default, null)), document.getElementById('root'));
-//# sourceMappingURL=index.js.map
