84a7a645f277379ce1994ce461ff37b105fd1954
Lukas Jakob <lu.jakob@googlemail.com>
2017-11-04 19:34:20 +0100

    Refactor ride-list

--- client/src/components/RideList.js
+++ client/src/components/RideList.js
@@ -9,12 +9,18 @@ class RideList extends Component {
     return (
       <div className="ride-list">
         {this.props.rides && this.props.rides.map((ride, index) => (
-          <div className="ride-item" key={index} index={index}>
-            <div className="fl w-20"><Link to={`/ride/${ride.id}`}>{ride.start}</Link></div>
-            <div className="fl w-20"><Link to={`/ride/${ride.id}`}>{ride.end}</Link></div>
-            <div className="fl w-20">{moment(new Date(ride.startDate)).format('D.M.Y')}</div>
-            <div className="fl w-20">{moment(new Date(ride.startDate)).format('kk:mm')} Uhr</div>
-            <div className="fl w-20">{ride.activity}</div>
+          <div className="ride-item cf" key={index} index={index}>
+            <Link to={`/ride/${ride.id}`}></Link>
+            <div className="ride-item-start-date">
+              {moment(new Date(ride.startDate)).format('ddd D.MMM')} - {moment(new Date(ride.startDate)).format('kk:mm')} Uhr
+            </div>
+
+            <div className="ride-item-start-end">
+              {ride.start} - {ride.end}
+            </div>
+            <div className="ride-item-activity">
+              {ride.activity}
+            </div>
           </div>
         ))
         }
