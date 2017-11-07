63b22e3df15e0ad4d96fc4345aba25b1d6970dcd
Lukas Jakob <lu.jakob@googlemail.com>
2017-11-07 10:20:18 +0100

    Refactor ride-list-items into separate component

--- client/src/components/RideList.js
+++ client/src/components/RideList.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
-import moment from 'moment'
-import 'moment/locale/de'
+import RideListItem from './RideListItem'
 
 class RideList extends Component {
 
@@ -9,19 +7,7 @@ class RideList extends Component {
     return (
       <div className="ride-list">
         {this.props.rides && this.props.rides.map((ride, index) => (
-          <div className="ride-item cf" key={index} index={index}>
-            <Link to={`/ride/${ride.id}`}></Link>
-            <div className="ride-item-start-date">
-              {moment(new Date(ride.startDate)).format('ddd D.MMM')} - {moment(new Date(ride.startDate)).format('kk:mm')} Uhr
-            </div>
-
-            <div className="ride-item-start-end">
-              {ride.start} - {ride.end}
-            </div>
-            <div className="ride-item-activity">
-              {ride.activity}
-            </div>
-          </div>
+          <RideListItem ride={ride}/>
         ))
         }
       </div>
---
+++ client/src/components/RideListItem.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import moment from 'moment'
+import 'moment/locale/de'
+
+class RideListItem extends Component {
+
+  render () {
+    return (
+      <div className="ride-list-item cf">
+        <Link to={`/ride/${this.props.ride.id}`}>
+          <div className="ride-list-item__start-date">
+            {moment(new Date(this.props.ride.startDate)).format('ddd D.MMM')} - {moment(new Date(this.props.ride.startDate)).format('kk:mm')} Uhr
+          </div>
+
+          <div className="ride-list-item__start-end">
+            {this.props.ride.start} - {this.props.ride.end}
+          </div>
+
+          <div className="ride-list-item__activity">
+            {this.props.ride.activity}
+          </div>
+        </Link>
+      </div>
+    )
+  }
+};
+
+export default RideListItem
