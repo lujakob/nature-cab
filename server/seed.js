6705165f281ff967b2f08b9dc978b4c92c56cb59
Lukas Jakob <lu.jakob@googlemail.com>
2017-11-08 11:59:25 +0100

    Refactor seed to store user._id for rides

--- server/seed.js
+++ server/seed.js
@@ -8,7 +8,8 @@ import 'moment/locale/de'
 import {saltRounds, ACTIVITIES} from './constants'
 
 // store save promises here to exit after all resolved
-let savePromises = []
+let userSavePromises = []
+let rideSavePromises = []
 
 mongoose.connect('mongodb://localhost:27017/naturecab', {useMongoClient: true})
 const db = mongoose.connection
@@ -34,6 +35,16 @@ db.dropCollection('UserList').then((err, result) => {
     console.log('+++Drop collection UserList successfully')
   }
 })
+
+// drop UserList collection
+db.dropCollection('RideList').then((err, result) => {
+  if (err) {
+    console.log('---Drop collection RideList failed ' + err)
+  } else {
+    console.log('+++Drop collection RideList')
+  }
+})
+
 
 // insert users into UserList
 const users = [
@@ -61,47 +72,48 @@ users.map(user => {
   })
 
   // store save promises here to exit after all resolved
-  savePromises.push(savePromise)
+  userSavePromises.push(savePromise)
 
 })
 
-// drop UserList collection
-db.dropCollection('RideList').then((err, result) => {
-  if (err) {
-    console.log('---Drop collection RideList failed ' + err)
-  } else {
-    console.log('+++Drop collection RideList')
-  }
-})
 
+// create rides, once the users are created
+Promise.all(userSavePromises).then(() => {
+  // insert users into UserList
+  moment.locale('de')
 
-// insert users into UserList
-moment.locale('de')
-
-const rideList = [
-  {userId: 1, start: 'Munich', end: 'Garmisch', seats: 3, price: 20, activity: ACTIVITIES.HIKE, id: 1, startDate: moment().add(-1, 'days').startOf('hour').toDate()},
-  {userId: 2, start: 'Munich', end: 'Tegernsee', seats: 2, price: 28, activity: ACTIVITIES.HIKE, id: 2, startDate: moment().startOf('hour').toDate()},
-  {userId: 1, start: 'Augsburg', end: 'Spitzingsee', seats: 2, price: 28, activity: ACTIVITIES.TRAILRUN, id: 3, startDate: moment().add(4, 'days').startOf('hour').toDate()},
-  {userId: 1, start: 'Munich', end: 'Garmisch', seats: 2, price: 30, activity: ACTIVITIES.TRAILRUN, id: 4, startDate: moment().add({days: 4, hours: 2}).startOf('hour').toDate()}
-]
-
-rideList.map(ride => {
-  let newRide = new RIDE(ride)
-
-  let savePromise = newRide.save((err, result) => {
-    if (err) {
-      console.log('---Ride save failed ' + err)
+  USER.find((err, users) => {
+    if(err) {
+      console.log('--- failed to find all Users')
     } else {
-      console.log('+++Ride ' + result.id + ' saved.')
+
+      const rideList = [
+        {userId: 1, start: 'Munich', end: 'Garmisch', seats: 3, price: 20, activity: ACTIVITIES.HIKE, id: 1, startDate: moment().add(-1, 'days').startOf('hour').toDate(), user: users[0]._id},
+        {userId: 2, start: 'Munich', end: 'Tegernsee', seats: 2, price: 28, activity: ACTIVITIES.HIKE, id: 2, startDate: moment().startOf('hour').toDate(), user: users[1]._id},
+        {userId: 1, start: 'Augsburg', end: 'Spitzingsee', seats: 2, price: 28, activity: ACTIVITIES.TRAILRUN, id: 3, startDate: moment().add(4, 'days').startOf('hour').toDate(), user: users[0]._id},
+        {userId: 1, start: 'Munich', end: 'Garmisch', seats: 2, price: 30, activity: ACTIVITIES.TRAILRUN, id: 4, startDate: moment().add({days: 4, hours: 2}).startOf('hour').toDate(), user: users[0]._id}
+      ]
+
+      rideList.map(ride => {
+        let newRide = new RIDE(ride)
+
+        let rideSavePromise = newRide.save((err, result) => {
+          if (err) {
+            console.log('---Ride save failed ' + err)
+          } else {
+            console.log('+++Ride ' + result.id + ' saved.')
+          }
+        })
+
+        // store save promises here to exit after all resolved
+        rideSavePromises.push(rideSavePromise)
+
+      })
+
+      // disconnect to exit node script, when all save's are resolved
+      Promise.all(rideSavePromises).then(() => {
+        mongoose.disconnect()
+      })
     }
   })
-
-  // store save promises here to exit after all resolved
-  savePromises.push(savePromise)
-
-})
-
-// disconnect to exit node script, when all save's are resolved
-Promise.all(savePromises).then(() => {
-  mongoose.disconnect()
 })
