3643ad2e3705910e28ff530105d2cf052c53b32c
Lukas Jakob <lu.jakob@googlemail.com>
2017-11-21 10:37:59 +0100

    Remove leftovers

--- client/src/components/Ride/RideUser.js
+++ client/src/components/Ride/RideUser.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react'
 import {getAgeFromYearOfBirth, truncateName} from '../../utils/misc'
-import {VEHICLES} from '../../constants'
-
 
 class RideUser extends Component {
 
