024967d2aa75ea4b2785da8ee18ad88af5354fb6
Lukas Jakob <lu.jakob@googlemail.com>
2017-11-22 11:22:58 +0100

    Add field error border color for register form

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -6,22 +6,25 @@ import {formIsValid, getYearOfBirthOptions} from '../utils/misc'
 import {LayoutLeftCol} from './Layout/LayoutLeftCol'
 import {Logo} from './Logo'
 
-const userSkipMandatoryFields = ['status']
+const userSkipMandatoryFields = []
 
-let stateDefaults = {
+let defaultUser = {
   gender: '',
   firstname: '',
   lastname: '',
   email: '',
   yearOfBirth: '',
-  password: '',
-  status: null
+  password: ''
 }
 
 class Register extends LayoutLeftCol {
-  state = Object.assign({}, stateDefaults)
+  state = {
+    user: Object.assign({}, defaultUser),
+    error: null
+  }
 
   render() {
+    const {user, error} = this.state
     return (
       <div className="register-page page-wrapper">
 
@@ -32,13 +35,13 @@ class Register extends LayoutLeftCol {
             Noch kein Mitglied?<br/>
             Jetzt kostenlos anmelden.
           </h2>
-          <div className="form-row">
+          <div className={'form-row ' + this._getErrorClass('gender')}>
             <input
               id="registration-gender-0"
               type="radio"
               value="male"
               name="gender"
-              onChange={(evt) => this.setState({gender: evt.target.value, status: null})}
+              onChange={this._setFieldValue}
             />
             <label className="radio-label" htmlFor="registration-gender-0">Herr</label>
             <input
@@ -46,50 +49,54 @@ class Register extends LayoutLeftCol {
               type="radio"
               value="female"
               name="gender"
-              onChange={(evt) => this.setState({gender: evt.target.value, status: null})}
+              onChange={this._setFieldValue}
             />
             <label className="radio-label" htmlFor="registration-gender-1">Frau</label>
 
           </div>
-          <div className="form-row">
+          <div className={'form-row ' + this._getErrorClass('firstname')}>
             <input
               type="text"
               placeholder="Vorname"
-              value={this.state.firstname}
-              onChange={(evt) => this.setState({firstname: evt.target.value, status: null})}
+              value={user.firstname}
+              name="firstname"
+              onChange={this._setFieldValue}
             />
           </div>
-          <div className="form-row">
+          <div className={'form-row ' + this._getErrorClass('lastname')}>
             <input
               type="text"
               placeholder="Nachname"
-              value={this.state.lastname}
-              onChange={(evt) => this.setState({lastname: evt.target.value, status: null})}
+              value={user.lastname}
+              name="lastname"
+              onChange={this._setFieldValue}
             />
           </div>
 
-          <div className="form-row">
+          <div className={'form-row ' + this._getErrorClass('email')}>
             <input
               type="text"
               placeholder="Email"
-              value={this.state.email}
-              onChange={(evt) => this.setState({email: evt.target.value, status: null})}
+              value={user.email}
+              name="email"
+              onChange={this._setFieldValue}
             />
           </div>
 
-          <div className="form-row">
+          <div className={'form-row ' + this._getErrorClass('password')}>
             <input
               type="text"
               placeholder="Password"
-              value={this.state.password}
-              onChange={(evt) => this.setState({password: evt.target.value, status: null})}
+              value={user.password}
+              name="password"
+              onChange={this._setFieldValue}
             />
           </div>
 
-          <div className="form-row">
+          <div className={'form-row ' + this._getErrorClass('yearOfBirth')}>
             <select
-              onChange={(evt) => this.setState({yearOfBirth: evt.target.value, status: null})}
-              name="activity"
+              onChange={this._setFieldValue}
+              name="yearOfBirth"
             >
               <option value="">Geburtsjahr</option>
               {getYearOfBirthOptions().map((year, index) => {
@@ -98,45 +105,64 @@ class Register extends LayoutLeftCol {
             </select>
           </div>
 
+          {error === VALIDATION.VALIDATION_REQUIRED &&
+          <div className="form-row error-message dark-red">
+            Bitte füllen Sie alle Felder aus.
+          </div>
+          }
+
+          {error === VALIDATION.VALIDATION_EMAIL_UNIQUE &&
+          <div className="form-row error-message dark-red">
+            Diese Email ist schon registriert.
+          </div>
+          }
+
           <div className="form-row form-row--button-right">
             <button
-              className='f6 link br3 ba ph3 pv2 mb2 dib white bg-blue'
+              className='link ph3 pv2 white bg-blue'
               onClick={() => this._submit()}
             >
               Registrieren
             </button>
           </div>
         </fieldset>
-
-        {this.state.status === VALIDATION.VALIDATION_REQUIRED &&
-        <div className="error-message dark-red">
-          Bitte füllen Sie alle Felder aus.
-        </div>
-        }
-
-        {this.state.status === VALIDATION.VALIDATION_EMAIL_UNIQUE &&
-        <div className="error-message dark-red">
-          Diese Email ist schon registriert.
-        </div>
-        }
       </div>
     )
   }
 
+  _getErrorClass() {
+    return this.state.error && !this.state.user[field] ? 'is-error' : ''
+  }
+
+  /**
+   * set field value on state, reset error prop
+   * @param evt
+   * @private
+   */
+  _setFieldValue = (evt) => {
+    let {name, value} = evt.target
+
+    let newState = Object.assign({}, this.state, {error: null})
+    newState['user'][name] = value
+    this.setState(newState)
+  }
+
   _submit = async () => {
 
-    if (!formIsValid(this.state, userSkipMandatoryFields)) {
-      this.setState({status: VALIDATION.VALIDATION_REQUIRED})
+    const {user} = this.state
+
+    if (!formIsValid(user, userSkipMandatoryFields)) {
+      this.setState({error: VALIDATION.VALIDATION_REQUIRED})
       return
     }
 
     let newUser = {
-      gender: this.state.gender,
-      firstname: this.state.firstname,
-      lastname: this.state.lastname,
-      email: this.state.email,
-      password: this.state.password,
-      yearOfBirth: this.state.yearOfBirth
+      gender: user.gender,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email,
+      password: user.password,
+      yearOfBirth: user.yearOfBirth
     }
 
     await this.props.createUserMutation({
@@ -144,12 +170,12 @@ class Register extends LayoutLeftCol {
       update: (store, {data: {createUser}}) => {}
     })
     .then(() => {
-      this.setState(stateDefaults)
+      this.setState({user: defaultUser})
       this.props.history.push('/login')
     })
     .catch(err => {
       if (err.message.indexOf(VALIDATION.VALIDATION_EMAIL_UNIQUE) >= 0) {
-        this.setState({status: VALIDATION.VALIDATION_EMAIL_UNIQUE})
+        this.setState({error: VALIDATION.VALIDATION_EMAIL_UNIQUE})
       }
       console.log('error', err)
     })
